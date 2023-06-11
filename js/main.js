de0e8f6d64716dfa6586ed9313721b872441b6ca
Kwinten De Deyn <kwintendedeyn@gmail.com>
2023-06-11 15:58:19 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const menuBtn = document.querySelector('#menu');
+const nav = document.querySelector('nav');
+
+menuBtn.addEventListener('click', function () {
+
+    const expanded = this.getAttribute('aria-expanded') === "true";
+    this.setAttribute('aria-expanded', !expanded);
+
+    if (expanded) {
+        nav.classList.remove('visible')
+        nav.classList.add('hidden')
+    } else {
+        nav.classList.remove('hidden')
+        nav.classList.add('visible')
+    }
+})
