dd5eff1f6e587d3219f462a5b6833bd1d540c6ff
Kwinten De Deyn <kwintendedeyn@gmail.com>
2023-06-11 16:10:23 +0200

    Tekstjes veranderdd

--- js/main.js
+++ js/main.js
@@ -1,16 +1,2 @@
-const menuBtn = document.querySelector('#menu');
-const nav = document.querySelector('nav');
-
-menuBtn.addEventListener('click', function () {
-
-    const expanded = this.getAttribute('aria-expanded') === "true";
-    this.setAttribute('aria-expanded', !expanded);
-
-    if (expanded) {
-        nav.classList.remove('visible')
-        nav.classList.add('hidden')
-    } else {
-        nav.classList.remove('hidden')
-        nav.classList.add('visible')
-    }
-})
+import { attachHeaderMenu } from "./headers";
+attachHeaderMenu();
